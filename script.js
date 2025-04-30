b6f8e1de59569bd36f36f21e7b430275da28bce6
Renata Silva <r.ariane@live.com>
2025-04-30 17:17:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function insertToScreen(data){
+    document.querySelector('#screen').value += data
+}
+
+function clean(){
+    document.querySelector('#screen').value = ''
+}
+
+function backspace(){
+    const screen = document.querySelector('#screen')
+    screen.value = screen.value.slice(0,-1)
+}
+
+function calcular(){
+    const screen = document.querySelector('#screen')
+    try {
+        screen.value = eval(screen.value)
+    } catch {
+        screen.value = 'Erro'
+    }
+}
+
+function raiz() {
+    const screen = document.querySelector('#screen');
+    try {
+        screen.value = Math.sqrt(eval(screen.value));
+    } catch {
+        screen.value = 'Erro';
+    }
+}
+
+function porcentagem() {
+    const screen = document.querySelector('#screen');
+    try {
+        screen.value = eval(screen.value) / 100;
+    } catch {
+        screen.value = 'Erro';
+    }
+}
